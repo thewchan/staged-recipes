1699d76c96330c0db7045c9c8e11c8d771eb1fc6
Matt Chan <thewchan@gmail.com>
2022-11-25 21:12:59 -0500

    Added r-zcurve as a pacakge

--- recipes/r-zcurve/build.sh
+++ recipes/r-zcurve/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 export DISABLE_AUTOBREW=1
-mv DESCRIPTION DESCRIPTION.old
-grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
-${R} CMD INSTALL --build . "${R_ARGS}"
+${R} CMD INSTALL --build . ${R_ARGS}
